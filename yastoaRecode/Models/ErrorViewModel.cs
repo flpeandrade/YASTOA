170a7ef299886467d550ebcd760c086214b6b0a1
Felipe <felipeandradecastro@gmail.com>
2022-01-04 11:45:48 -0300

    first commit

---
+++ yastoaRecode/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace yastoaRecode.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
