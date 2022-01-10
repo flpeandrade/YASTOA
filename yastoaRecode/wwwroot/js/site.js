6994bef36b971772f963a53389b29d3355963908
Felipe <felipeandradecastro@gmail.com>
2022-01-10 13:04:57 -0300

    Paginação

--- yastoaRecode/wwwroot/js/site.js
+++ yastoaRecode/wwwroot/js/site.js
@@ -15,3 +15,32 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+let time = 5000,
+    currentImageIndex = 0,
+    images = document
+        .querySelectorAll("#hero img")
+max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("ativo")
+
+    currentImageIndex++
+
+    if (currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("ativo")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
