ac934abf4f91f44b0d80d1ddd50c52a622d8c24e
Overthrive <sara.leal.lessa@gmail.com>
2022-02-18 08:48:55 -0300

    hospedado

--- yastoaRecode/Areas/Identity/IdentityHostingStartup.cs
+++ yastoaRecode/Areas/Identity/IdentityHostingStartup.cs
@@ -17,7 +17,7 @@ namespace yastoaRecode.Areas.Identity
         {
             builder.ConfigureServices((context, services) => {
                 services.AddDbContext<YastoaRecodeDbContext>(options =>
-                    options.UseSqlServer(context.Configuration.GetConnectionString("Context")));
+                    options.UseSqlServer(context.Configuration.GetConnectionString("ConnectionStringConnect")));
 
                 services.AddDefaultIdentity<AplicationUser>(options => {
                     options.SignIn.RequireConfirmedAccount = false;
